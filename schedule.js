6a169a8edd781ec4da647ad67a329ae2f27c0783
WhatTheVlad <mocanuvlad_ics@yahoo.com>
2020-06-03 14:36:05 +0300

    implemented rest check and scheduling logic

--- schedule.js
+++ schedule.js
@@ -1,24 +1,9 @@
 
-
 /*
     Mock Variables
 */
 
-let prevDayObj = {
-    '01.01.2020': {
-        'day': 'Emp1',
-        'night': ''
-    }
-}
-
-let prevDay2Obj = {
-    '02.01.2020': {
-        'day': '',
-        'night': ''
-    }
-}
-
-let emptySchedule = {
+const emptySchedule = {
     '01.01.2020': {
         'day': '',
         'night': ''
@@ -40,84 +25,107 @@ let emptySchedule = {
         'night': ''
     }
 }
-let employeeList = ['Emp 1', 'Emp 2', 'Emp 3', 'Emp 4'];
+const employeeList = ['Emp 1', 'Emp 2', 'Emp 3', 'Emp 4'];
 
 
 /*
     App Variables
 */
-let finalSchedule;
-
+const DAY = 'day';
+const NIGHT = 'night';
 
 /*
     Util Functions
 */
 
 // Main function. Computes schedule according to all rules and filters.
-function computeSchedule(emptySchedule, employeeList) {
-    let newSchedule = emptySchedule;
-    let workdaysEntries = Object.entries(emptySchedule);
-    let workdaysKeys = Object.keys(emptySchedule);
+function computeSchedule(initialSchedule, employee) {
+    let newSchedule = initialSchedule;
+    let workdaysKeys = Object.keys(initialSchedule);
 
-    Object.entries(emptySchedule).map(element => {
+    Object.entries(newSchedule).map(element => {
 
         let workday = element[0];
-        let dayShift = element[1].day;
-        let nightShift = element[1].night;
+        let dayShift = element[1][DAY];
+        let nightShift = element[1][NIGHT];
         let workdayIndex = workdaysKeys.indexOf(workday)
+        let currentDay = Object.entries(newSchedule)[workdayIndex];
+        let prevDay = Object.entries(newSchedule)[workdayIndex - 1];
+        let prevDay2 = Object.entries(newSchedule)[workdayIndex - 2];
 
         if (workdayIndex > 1) {
             if (dayShift == '') {
-                let canEmpWork = checkRestTimeForDay('Emp1', prevDayObj, prevDay2Obj);
-                if (canEmpWork) {
-                    newSchedule[workday]['day'] = employeeList[0]
+                let canEmpWorkDay = checkDayShiftAvailability(employee, prevDay, prevDay2);
+                if (canEmpWorkDay) {
+                    newSchedule[workday][DAY] = employee;
                 }
             }
 
             if (nightShift == '') {
-                //nightShift = employeeList[0];
+                let canEmpWorkNight = checkNightShiftAvailability(employee, currentDay, prevDay, prevDay2);
+                if (canEmpWorkNight) {
+                    newSchedule[workday][NIGHT] = employee;
+                }
             }
         }
     })
-
+    return newSchedule;
 }
 
-/** Checks employee availability for a day shift */
-function checkRestTimeForDay(employee, prevDayObj, prevDay2Obj) {
+/** Checks employee availability for day shifts */
+function checkDayShiftAvailability(employee, prevDay, prevDay2) {
+    let prevDayShifts = prevDay[1];
+    let prevDay2Shifts = prevDay2[1];
+    let restShifts = [prevDayShifts[NIGHT], prevDayShifts[DAY], prevDay2Shifts[NIGHT]]
 
-    let prevDayShifts = Object.entries(prevDayObj[Object.keys(prevDayObj)[0]]);
-    let prevDay2Shifts = Object.entries(prevDay2Obj[Object.keys(prevDay2Obj)[0]]);
-    let restShifts = [prevDayShifts[0], prevDayShifts[1], prevDay2Shifts[1]]
-
-    restShifts.forEach(shift => {
-        if (shift[1] == employee) {
-            return false;
-        }
-    });
-
-    return true;
+    if (!restShifts.includes(employee)) {
+        return true
+    } else {
+        return false
+    }
 }
 
-/** Checks employee availability for a night shift */
-function checkRestTimeForNight(employee, prevDayObj, prevDay2Obj) {
+/** Checks employee availability for night shifts */
+function checkNightShiftAvailability(employee, currentDay, prevDay, prevDay2) {
 
-    let prevDayShifts = Object.entries(prevDayObj[Object.keys(prevDayObj)[0]]);
-    let prevDay2Shifts = Object.entries(prevDay2Obj[Object.keys(prevDay2Obj)[0]]);
-    let restShifts = [prevDayShifts[0], prevDayShifts[1], prevDay2Shifts[1]]
+    let currentDayShifts = currentDay[1];
+    let prevDayShifts = prevDay[1];
+    let prevDay2Shifts = prevDay2[1];
+    let restShifts = [currentDayShifts[DAY], prevDayShifts[NIGHT], prevDay2Shifts[NIGHT]];
 
-    restShifts.forEach(shift => {
-        if (shift[1] == employee) {
-            return false;
-        }
-    });
-
-    return true;
+    if (!restShifts.includes(employee)) {
+        return true
+    } else {
+        return false
+    }
 }
 
+//Schedules employees for the first two days
+function scheduleFirstTwoDays(schedule, firstDay, secondDay, emp1, emp2, emp3) {
+    schedule[firstDay][DAY] = emp1;
+    schedule[firstDay][NIGHT] = emp2;
+    schedule[secondDay][DAY] = emp3;
+    schedule[secondDay][NIGHT] = emp1;
+
+    return schedule
+}
 
 /*
     Execution
 */
 
+scheduleFirstTwoDays(emptySchedule, '01.01.2020', '02.01.2020', 'Emp1', 'Emp2', 'Emp3')
+computeSchedule(emptySchedule, 'Emp1');
+computeSchedule(emptySchedule, 'Emp2');
+computeSchedule(emptySchedule, 'Emp3');
+computeSchedule(emptySchedule, 'Emp4');
 
-computeSchedule(emptySchedule, employeeList);
+console.log(emptySchedule)
+
+
+
+
+
+
+
+
